3df3d9a8d83b4b89629092868b1c5b79d281668b
Zageron <hello@adambryant.ca>
2021-02-15 15:13:23 -0800

    Add line intersection and tests.

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,21 @@ impl RoundToPlace for f64 {
     }
 }
 
+trait Validity {
+    fn is_valid(&self) -> bool;
+}
+
+impl Validity for DVec2 {
+    fn is_valid(&self) -> bool {
+        const INVALID_DVEC2: DVec2 = DVec2 {
+            x: f64::MAX,
+            y: f64::MAX,
+        };
+
+        !INVALID_DVEC2.eq(self)
+    }
+}
+
 // Wall consists of two points, a point a and a point b.
 // Should we define a wall as a point, a length, and an orientation?
 
@@ -75,9 +90,37 @@ fn parse_input() -> String {
     input
 }
 
+fn perpendicular(vector: DVec2) -> DVec2 {
+    DVec2 {
+        x: -vector.y,
+        y: vector.x,
+    }
+}
+
+fn line_intersection(pt0a: DVec2, pt0b: DVec2, pt1a: DVec2, pt1b: DVec2) -> DVec2 {
+    let delta_pt_0: DVec2 = pt0b - pt0a;
+    let delta_pt_1: DVec2 = pt1b - pt1a;
+    let delta_pt_a: DVec2 = pt0a - pt1a;
+
+    let perpendicular_delta_pt_0: DVec2 = perpendicular(delta_pt_0);
+    let denominator = perpendicular_delta_pt_0.dot(delta_pt_1);
+    let numerator = perpendicular_delta_pt_0.dot(delta_pt_a);
+
+    if denominator == 0.0 || numerator == 0.0 {
+        return DVec2 {
+            x: f64::MAX,
+            y: f64::MAX,
+        };
+    }
+
+    ((numerator / denominator) * delta_pt_1) + pt1a
+}
+
 fn main() {
-    let wall = Wall::new(0.0, 0.0, 10.0, 0.0);
-    println!("{}", wall);
+    let wall0: Wall = Wall::new(0.0, 0.0, 6.0, 0.0);
+    let wall1: Wall = Wall::new(0.0, 0.0, 6.0, 90.0);
+    println!("{}", wall0);
+    println!("{}", wall1);
 
     loop {
         println!("Example output for user to see for now: ");
@@ -88,8 +131,31 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::Wall;
+    use crate::Validity;
+    use crate::{line_intersection, Wall};
     use ultraviolet::DVec2;
+
+    #[test]
+    fn test_line_intersection_success() {
+        let pt0a: DVec2 = DVec2::new(0.0, 0.0);
+        let pt0b: DVec2 = DVec2::new(6.0, 6.0);
+        let pt1a: DVec2 = DVec2::new(0.0, 6.0);
+        let pt1b: DVec2 = DVec2::new(6.0, 0.0);
+
+        let intersection_pt = line_intersection(pt0a, pt0b, pt1a, pt1b);
+        assert_eq!(intersection_pt, DVec2 { x: 3.0, y: 3.0 })
+    }
+
+    #[test]
+    fn test_line_intersection_failure() {
+        let pt0a: DVec2 = DVec2::new(0.0, 0.0);
+        let pt0b: DVec2 = DVec2::new(6.0, 6.0);
+        let pt1a: DVec2 = DVec2::new(-6.0, -6.0);
+        let pt1b: DVec2 = DVec2::new(-6.0, -10.0);
+
+        let intersection_pt = line_intersection(pt0a, pt0b, pt1a, pt1b);
+        assert!(!intersection_pt.is_valid());
+    }
 
     #[test]
     fn create_wall_from_point_length_and_orientation() {
