8cc285cd17d900c955ececbf36b92ecccd8dd796
Zageron <hello@adambryant.ca>
2021-03-22 17:46:07 -0700

    Add short todo list in comments.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,26 @@ fn tick() {
     println!("Arbitrary tick.");
 }
 
+// Initialize app state control.
+//  - Pre-Game
+//  - Active game
+//  - Post-Game
+
+// Initialize game state control.
+//  - Player 1 and 2 scores.
+//  - Player 1 and 2 paddle positions.
+//  - Ball position and initial direction vector. (Angle of bounce)
+//  - Ball "speed" setting.
+//  - Current Turn / Turn Counters
+//  - Game State: Turn Start vs Active
+
+// Set up game rules:
+//  - Boundaries for "turn end"
+//  - Turn termination on boundary.
+//  - Interception logic for paddles. (Reflection)
+//  - Interception logic for top and bottom walls. (Reflection)
+//  - Wait for interaction to fire the pong ball at turn start.
+
 fn main() -> Result<()> {
     let wall0: Wall = Wall::new(0.0, 0.0, 6.0, 0.0);
     let wall1: Wall = Wall::new(0.0, 0.0, 6.0, 90.0);
