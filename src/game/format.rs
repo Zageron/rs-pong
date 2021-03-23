562f8beb15d3906966c41d4a025a509b1af67bf2
Zageron <hello@adambryant.ca>
2021-03-22 17:59:28 -0700

    Add format print for GameState.

--- src/game/format.rs
+++ src/game/format.rs
@@ -1,9 +1,19 @@
-pub use super::types::Wall;
+pub use super::types::{GameState, Wall};
 
 use std::fmt;
 
 impl fmt::Display for Wall {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?}, {:?})", self.point_a, self.point_b)
+    }
+}
+
+impl fmt::Display for GameState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "(P1: {:?}, P2: {:?})",
+            self.player0_score, self.player1_score
+        )
     }
 }
